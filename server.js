d4cbac75b7eefdcd2fceeb79d328096d79c9a064
Qasim2026 <mohamdigasim@gmail.com>
2026-08-29 19:57:05 +0330

    Update server.js

--- server.js
+++ server.js
@@ -602,7 +602,7 @@ async function ensureColumn(table, column, definition) {
 
 async function createTables() {
 
-  await pool.query(\`
+  await pool.query(`
     CREATE TABLE IF NOT EXISTS users (
       id SERIAL PRIMARY KEY,
       name TEXT NOT NULL,
